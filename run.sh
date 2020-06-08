636e6691cfbad57d3adbd83dc43fa94cc3a5f80d
EnricoRuggiano <enricoruggiano1@gmail.it>
2020-06-08 16:37:55 +0200

    [fix] some stuff on logs

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# clean log files
+if [-d ./delivery/SASCampaignNavigator/logs]
+then 
+    rm -r ./delivery/SASCampaignNavigator/logs/*
+fi
+
 # execute the delivery program
 ./delivery/SASCampaignNavigator/bin/SASCampaignNavigator
