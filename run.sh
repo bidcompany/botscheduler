f3a4dc713a65d7b660da7a0fdb529a0539754ce8
EnricoRuggiano <enricoruggiano1@gmail.it>
2020-06-08 16:40:29 +0200

    [fix] bash run.sh fix

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # clean log files
-if [-d ./delivery/SASCampaignNavigator/logs]
+if [[ -d ./delivery/SASCampaignNavigator/logs ]]
 then 
     rm -r ./delivery/SASCampaignNavigator/logs/*
 fi
