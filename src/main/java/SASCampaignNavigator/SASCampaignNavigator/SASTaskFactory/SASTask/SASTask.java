ec13f4ee695260d62f869cd2d89c990fe6e67eb4
EnricoRuggiano <enricoruggiano1@gmail.it>
2020-07-06 15:36:15 +0200

    [wip] guard on send schedule

--- src/main/java/SASCampaignNavigator/SASCampaignNavigator/SASTaskFactory/SASTask/SASTask.java
+++ src/main/java/SASCampaignNavigator/SASCampaignNavigator/SASTaskFactory/SASTask/SASTask.java
@@ -263,6 +263,12 @@ public abstract class SASTask
         found = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(toFind)));  
         found.click();
 
+        // pause
+        toFind = "//*[@title='Please wait']";
+        msg = "Wait untill the busy overlay is invisible";
+        logger.debug(msg);
+        logger.debug("xpath]: " + toFind);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(toFind)));
 
         // click on Close button for a cleaner ending.
         toFind = "//*[contains(text(), 'Close')]/ancestor::button[ancestor::header[//div[@role='toolbar']]]";
