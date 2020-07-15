9524869e2afc6ad8353efc45719285c19f79de79
EnricoRuggiano <enricoruggiano1@gmail.it>
2020-07-15 09:12:40 +0200

    [fix] ISP patch

--- ISP.bat
+++ ISP.bat
@@ -1,9 +1,9 @@
 echo 'Copy the delivery folder in ISP'
-xcopy /s/Y delivery %HOMEPATH%
+xcopy /s/Y %~dp0\delivery %HOMEPATH%
 
 echo 'Overwrite specifics setup file for ISP'
-xcopy /Y ISP\chromedriver.exe %HOMEPATH%\SASCampaignNavigator\lib\chromedriver/exe
-xcopy /Y ISP\SASCampaignNavigator.cfg %HOMEPATH%\SASCampaignNavigator\bin\SASCampaignNavigator.cfg
+xcopy /Y %~dp0\ISP\chromedriver.exe %HOMEPATH%\SASCampaignNavigator\lib\chromedriver.exe
+xcopy /Y %~dp0\ISP\SASCampaignNavigator.cfg %HOMEPATH%\SASCampaignNavigator\bin\SASCampaignNavigator.cfg
 
 echo 'Refresh session files for the new execution in ISP'
 if exist %HOMEPATH%\SASCampaignNavigator\bin\campaigns.json del /F %HOMEPATH%\SASCampaignNavigator\bin\campaigns.json
